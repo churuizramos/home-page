82f9ff81a67850e5232aaedff23f34cfa0440dd8
churuizramos <ruizramos04@outlook.com>
2024-12-20 11:10:51 -0500

    added Vue Router integreation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView, RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <HelloWorld/>
+  <nav class="flex flex-row justify-between p-5 text-xl bg-cyan-950">
+    <div class="bg-black p-4 rounded-full"></div>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/contact">Contact</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
-
-<style scoped>
-
-</style>
